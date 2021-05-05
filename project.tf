6009886a229f27bfdf73fc866c37b9996d6eb7c4
nazy67 <nazykh67@gmail.com>
2021-05-04 22:24:02 -0500

    modified project.tf

--- project.tf
+++ project.tf
@@ -1,15 +1,15 @@
 resource "digitalocean_project" "terraform" {
-  name        = "${var.env}-terraform"
-  description = "A project to represent development resources."
-  purpose     = "Web Application"
-  environment = "Development"
+ name        = "${var.env}-terraform"
+ description = "A project to represent development resources."
+ purpose     = "Web Application"
+ environment = "Development"
 }
 
 resource "digitalocean_project_resources" "tf-project" {
-  project = digitalocean_project.terraform.id
-  resources = [
-    digitalocean_vpc.my-vpc.urn,
-    digitalocean_droplet.web.urn,
-    digitalocean_droplet.db.urn
-  ]
+ project = digitalocean_project.terraform.id
+ resources = [
+   digitalocean_vpc.my-vpc.urn,
+   digitalocean_droplet.web.urn,
+   digitalocean_droplet.db.urn
+ ]
 }
