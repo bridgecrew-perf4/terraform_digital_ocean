ff3c193d33b1f73b1332116cd3ca8b3bf0a0cda1
nazy67 <nazykh67@gmail.com>
2021-05-04 22:23:11 -0500

    modified droplet.tf

--- droplet.tf
+++ droplet.tf
@@ -1,10 +1,11 @@
 resource "digitalocean_droplet" "web" {
-  image  = data.digitalocean_image.ubuntu_image.id
+  image  = data.digitalocean_image.centos_image.id #data.digitalocean_image.ubuntu_image.id
   name   = "${var.env}-web"
   region = var.region_for_droplet
   size   = var.droplet_size
-  vpc_uuid = digitalocean_vpc.my-vpc.id
-  tags   = [digitalocean_tag.env.id, digitalocean_tag.project.id, digitalocean_tag.team.id]
+  ssh_keys = [data.digitalocean_ssh_key.ssh-key.id]
+  #vpc_uuid = digitalocean_vpc.my-vpc.id
+  tags   = [digitalocean_tag.env.id, digitalocean_tag.project.id, digitalocean_tag.web.id, digitalocean_tag.team.id]
 }
 
 resource "digitalocean_droplet" "db" {
@@ -12,6 +13,7 @@ resource "digitalocean_droplet" "db" {
   name   = "${var.env}-db"
   region = var.region_for_droplet
   size   = var.droplet_size
-  vpc_uuid = digitalocean_vpc.my-vpc.id
-  tags   = [digitalocean_tag.env.id, digitalocean_tag.project.id, digitalocean_tag.team.id]  
+  ssh_keys = [data.digitalocean_ssh_key.ssh-key.id]
+  #vpc_uuid = digitalocean_vpc.my-vpc.id
+  tags   = [digitalocean_tag.env.id, digitalocean_tag.project.id, digitalocean_tag.db.id, digitalocean_tag.team.id]  
 }
