9eec27f952c5c834f28294065c5c7ed8fdb1ec72
nazy67 <nazykh67@gmail.com>
2021-05-04 22:21:56 -0500

    modified vpc_do.tf

--- vpc_do.tf
+++ vpc_do.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_vpc" "my-vpc" {
-  name   = "project-network"
-  region = "nyc1"
-  ip_range = "10.0.0.0/24"
+ name   = "project-network"
+ region = "nyc1"
+ ip_range = "10.0.0.0/24"
 }
